513d9486da36197e5b3e359b69a0cda67a041f2f
Cristiano Vieira <47859813+profcristianovs@users.noreply.github.com>
2020-08-20 20:13:45 -0300

    Add files via upload

---
+++ codigo/distancia.js
@@ -0,0 +1,10 @@
+var x1 = 10;
+var x2 = 05;
+var y1 = 20;
+var y2 = 08;
+
+var d;
+
+d = Math.sqrt( (x2-x1)^2 + (y2-y1)^2 );
+
+console.log("A distancia entre os pontos e´ " + d);
---
+++ codigo/elevador.js
@@ -0,0 +1,14 @@
+var capacidade = 400;
+var p1 = 65;
+var p2 = 80;
+var p3 = 45;
+var p4 = 90;
+var p5 = 60;
+var peso = p1+p2+p3+p4+p5;
+
+if(capacidade > peso){
+	console.log("liberado");
+}
+else{
+	console.log("peso maximo excedido");
+}
